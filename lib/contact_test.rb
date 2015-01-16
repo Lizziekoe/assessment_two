55ec7c83a4f796b46b80ffc2cc10fdcf792d47f5
Lizzie <lizziefkoehler@gmail.com>
2015-01-15 18:03:29 -0800

    update contact class to accommodate only for contact names

--- lib/contact_test.rb
+++ lib/contact_test.rb
@@ -2,22 +2,22 @@ class Contact
 
 	@@all_contacts = []
 
-	attr_reader(:name, :number)
+	attr_reader(:name)
 
 	define_method(:initialize) do |attributes|
-		@name = attributes.fetch(:name)
-		@number = attributes.fetch(:number)		
+		@name = attributes.fetch(:name)	
 	end
+  
+  define_singleton_method(:all) do
+    @@all_contacts
+  end
 
 	define_method(:save) do
 		@@all_contacts.push(self)
 	end
 
-	define_singleton_method(:all) do
-		@@all_contacts
-	end
-
 	define_singleton_method(:clear) do
 		@@all_contacts = []
 	end
+
 end
--- spec/contact_test_spec.rb
+++ spec/contact_test_spec.rb
@@ -1,30 +1,40 @@
 require('rspec')
 require('contact_test')
+require('phone')
 
 describe ('Contact') do
     before() do
     Contact.clear()
   end
 
-  describe(".clear") do
-    it("clears the array") do
-      Contact.new({:name => "lizzie", :number => 5037998459}).save()
-      Contact.clear()
-      expect(Contact.all()).to(eq([]))
-    end
-  end
-
-  describe(".all") do
-    it("is empty at first") do
-      expect(Contact.all()).to(eq([]))
+  describe("#name") do
+    it("will return the name of the contact") do
+      test_contact = Contact.new({:name => "lizzie"})
+      expect(test_contact.name()).to(eq("lizzie"))
     end
   end
 
   describe("#save") do
-    it("saves another name and number to the phonebook") do
-      test_contact = Contact.new({:name => "lizzie", :number => 5037998459})
+    it("saves another name to the array of saved names") do
+      test_contact = Contact.new({:name => "lizzie"})
       test_contact.save()
     expect(Contact.all()).to(eq([test_contact]))
     end
   end
+
+  describe(".all") do
+    it("is empty before entering first contact") do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("clears the array") do
+      Contact.new({:name => "lizzie"}).save()
+      Contact.clear()
+      expect(Contact.all()).to(eq([]))
+    end
+  end
 end
+
+
