77af501cd682b062130a1167b6b588c968133159
Lizzie <lizziefkoehler@gmail.com>
2015-01-16 09:23:48 -0800

    add a form and some ruby to the index.erb file

--- app.rb
+++ app.rb
@@ -4,29 +4,7 @@ also_reload('lib/**/*.rb')
 require('./lib/contact_test')
 require('./lib/phone')
 
-get('/') do
-  @contacts = Contact.all()
-  erb(:index)
-end
-
-# post('/artists') do
-#   @artist_name = params.fetch('artist')
-#   Artist.new({:artist_name => @artist_name}).save()
-#   @artists = Artist.all()
-#   erb(:index)
-# end
-
-# get('/artists/:id') do
-#   @artists = Artist.find(params.fetch('id').to_i())
-#   @albums = Album.all()
-#   erb(:artists)
-# end
-
-# post('/albums') do
-#   @album_name = params.fetch('album')
-#   @albums = Album.new({:album_name => @album_name}).save()
-#   @artists = Artist.find(params.fetch('artist_id').to_i())
-#   @artists.add_album(@album_name)
-#   @albums = Album.all()
-#   erb(:artists)
-# end
+ get('/') do
+   @all_contacts = Contact.all()
+   erb(:index)
+ end
